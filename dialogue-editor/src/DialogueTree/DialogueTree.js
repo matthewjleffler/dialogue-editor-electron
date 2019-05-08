8a527ee0b1f8d93b3105e48b22b027baba1dc401
Matthew Leffler <matthewjleffler@gmail.com>
2019-05-08 11:11:15 -0700

    Added dialogue tree package

--- dialogue-editor/src/DialogueTree/DialogueTree.js
+++ dialogue-editor/src/DialogueTree/DialogueTree.js
@@ -1,19 +1,71 @@
 import React, { Component } from 'react';
+import '../../node_modules/react-ui-tree/dist/react-ui-tree.css'; // TODO this
 import './DialogueTree.css';
-import DialogueTreeLine from './DialogueTreeLine';
+import cx from 'classnames'
+import Tree from 'react-ui-tree';
 
 class DialogueTree extends Component {
+  constructor(props) {
+    super(props);
 
-  render() {
-
-    let list = [];
-    for (let i = 0; i < 50; i++) {
-      list.push(<DialogueTreeLine key={'treeline' + i} text={'val' + i}/>);
+    const tree = {
+      "module": "react-ui-tree",
+      "children": []
     }
 
+    for (let i = 0; i < 100; i++) {
+      tree.children.push(
+        {
+          "collapsed": true,
+          "module": "dist",
+          "children": [{
+            "module": "node.js"
+          }]
+        }
+      );
+    }
+
+    this.state = {
+      active: null,
+      tree: tree,
+    }
+  }
+
+  renderNode = node => {
+    return (
+      <span
+        className={cx('node', {
+          'is-active': node === this.state.active
+        })}
+        onClick={this.onClickNode.bind(null, node)}
+      >
+        {node.module}
+      </span>
+    );
+  };
+
+  onClickNode = node => {
+    this.setState({
+      active: node
+    });
+  };
+
+  handleChange = tree => {
+    this.setState({
+      tree: tree
+    });
+  };
+
+  render() {
     return (
       <div className="TreeContainer Scrolling">
-        {list}
+        <Tree
+          paddingLeft={20}
+          tree={this.state.tree}
+          onChange={this.handleChange}
+          isNodeCollapsed={this.isNodeCollapsed}
+          renderNode={this.renderNode}
+        />
       </div>
     );
   }
